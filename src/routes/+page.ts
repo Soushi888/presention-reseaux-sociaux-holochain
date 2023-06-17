cea38896dc4cc573ee5f06b190de2ab9e4c86a16
soushi888 <sacha.pignot@protonmail.com>
2023-06-17 01:13:28 -0400

    add internationalization feature

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,1 +1,10 @@
+import { languageStore } from '$lib/store';
+import type { PageLoad } from './$types';
+
 export const ssr = false;
+
+export const load: PageLoad = async ({ url }) => {
+	const lang = url.searchParams.get('lang') ?? 'en';
+
+	languageStore.set(lang);
+};
